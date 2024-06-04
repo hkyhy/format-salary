2eef5fa06db7e9dcfa5f36e5aac802b281caba81
jinfanghuang <2283785225@qq.com>
2024-06-04 14:01:46 +0800

    feat: format salary

---
+++ index.js
@@ -0,0 +1,21 @@
+const formatSalary = (salary_begin, salary_end, salaryType, salaryMonth) => {
+  if (salaryType === 1) {
+    return "面议"
+  }
+  if (salary_begin == 0 && salary_end > 0) {
+    return `${salary_end}K以内`;
+  } else if (salary_begin > 0 && salary_end > salary_begin) {
+    if(salaryMonth>12){
+      return `${salary_begin}-${salary_end}K*${salaryMonth}薪`;
+    }
+    return `${salary_begin}-${salary_end}K`
+  } else if (salary_begin > 0 && salary_end === salary_begin) {
+    return `${salary_begin}K`;
+  } else if (salary_begin > 0) {
+    return `${salary_begin}K以上`;
+  } else {
+    return false;
+  }
+};
+
+export default formatSalary;
